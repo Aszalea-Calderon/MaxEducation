503c8347a949ccf49688b933c2078ed40ecd26a4
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-12-02 10:33:15 -0800

    adding conditional rendering notes

--- app/src/Notes/UseState.js
+++ app/src/Notes/UseState.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import Person from "./Person/Person";
+import Person from "../Person/Person";
 
 const App = (props) => {
   const [personsState, setPersonState] = useState({

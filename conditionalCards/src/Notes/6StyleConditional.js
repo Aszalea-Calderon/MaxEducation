db23d945fe5c047d9f0bf52e944cc417685b0e60
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-12-03 10:20:13 -0800

    added style notes

--- conditionalCards/src/Notes/6StyleConditional.js
+++ conditionalCards/src/Notes/6StyleConditional.js
@@ -1,10 +1,9 @@
 import { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
+//This change changes when the button styles are being rendered
 
 class App extends Component {
-  //Older versions of react, this was the only way to manage state. But this is how you would work with it prior to this
-
   state = {
     persons: [
       { id: 1, name: "Aszalea", hobby: "cake" },
@@ -15,43 +14,32 @@ class App extends Component {
   };
 
   nameChangeHandler = (event, id) => {
-    //1 Here we are reaching out to the state to check the id so we can edit accordingly
     const personsIndex = this.state.persons.findIndex((p) => {
-      return p.id === id; //This is holding that data for now
+      return p.id === id;
     });
-
-    //2 This then checks the personIndex
     const person = { ...this.state.persons[personsIndex] };
-
-    //This could also be used instead of the above funciton but it is prefered to use the one above
-    //const person = Object.assign({}, this.state.persons[personIndex])
-
-    // 3  now we are assigning it to the name and updating who is being assigned
     person.name = event.target.value;
     const persons = [...this.state.persons];
     persons[personsIndex] = person;
 
-    //Then after it has been copied and updated we can set it to our current state
     this.setState({
       persons: persons,
     });
   };
 
   deletePersonsHandeler = (personsIndex) => {
-    //const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personsIndex, 1);
     this.setState({ persons: persons });
   };
 
-  //This sets if we can see the div below of not
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow });
   };
   render() {
     const style = {
-      //This is inline styling
+      //This is inline styling currently
       backgroundColor: "white",
       font: "inherit",
       border: "1px solid blue",
@@ -70,14 +58,14 @@ class App extends Component {
                 click={() => this.deletePersonsHandeler(index)}
                 name={person.name}
                 hobby={person.hobby}
-                key={person.id} //This is needed so the document understands what you are asking it to do, see personIndex
+                key={person.id}
                 changed={(event) => this.nameChangeHandler(event, person.id)}
               />
             );
           })}
         </div>
       );
-      style.backgroundColor = "red";
+      style.backgroundColor = "red"; //This is showing a red background on the button only when the button has been clicked
     }
 
     return (
@@ -85,7 +73,6 @@ class App extends Component {
         <h1>Hi</h1>
         <p>This is working!</p>
         <button style={style} onClick={this.togglePersonsHandler}>
-          {/* Note the toggle on the button */}
           Show friends
         </button>
         {persons}
